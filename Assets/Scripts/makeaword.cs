1541bd6900e4c315594e6abce16ed22edb44e532
Maria Lantin <maria.lantin@gmail.com>
2017-02-25 18:08:32 -0800

    Added the ability to make words in space. To try this out click and hold on the square and speak, the word will then appear in space and click again where you want to position it. It probably won’t work the first time…there is a timing issue with credentials I think. I next will work on getting the position of the pointer on the word when scanning over it.

---
+++ Assets/Scripts/makeaword.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class makeaword : MonoBehaviour {
+
+	public GameObject alphabet;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void makeword(string word, float scale, Vector3 pos, Quaternion rot, AudioClip clip) {
+		GameObject newword = new GameObject (word);
+		wordActs wordscript = newword.AddComponent<wordActs> ();
+		newword.transform.parent = transform;
+
+		GameObject newletter;
+		MeshFilter lettermesh;
+		Vector3 letterpos = new Vector3 ();
+		Vector3 scalevec = new Vector3 (scale, scale, scale);
+		Vector3 letterscale = new Vector3 (1f, 1f, 1f);
+
+		newword.transform.localScale = scalevec;
+		newword.transform.position = pos;
+		newword.transform.rotation = rot;
+
+		AudioSource wordsource = newword.AddComponent<AudioSource>();
+		wordsource.clip = clip;
+		wordsource.loop = false;
+
+
+		MeshFilter[] letters = alphabet.GetComponentsInChildren<MeshFilter> ();
+		Vector3 lettercentre;
+		Vector3 extent = new Vector3();
+		Vector3 boxsize = new Vector3 ();
+		foreach (char c in word) {
+			foreach (MeshFilter letter in letters) {
+				if (letter.name == c.ToString()) {
+					lettermesh = Instantiate(letter) as MeshFilter;
+					newletter = lettermesh.gameObject;
+					newletter.name = c.ToString ();
+					newletter.transform.parent = newword.transform;
+					newletter.transform.localScale = letterscale;
+					newletter.transform.localRotation = Quaternion.identity;
+					lettercentre = letter.sharedMesh.bounds.center;
+					extent = letter.sharedMesh.bounds.extents;
+					boxsize.Set (boxsize.x + extent.x * 2, Mathf.Max (boxsize.y, extent.y*2), Mathf.Max (boxsize.z, extent.z * 2));
+					newletter.transform.localPosition = letterpos-lettercentre+extent;
+					letterpos.x += extent.x*2;
+					break;
+				}
+			}
+		}
+		Rigidbody rb = newword.AddComponent<Rigidbody> ();
+		rb.useGravity = false;
+		rb.constraints = RigidbodyConstraints.FreezeAll;
+
+		BoxCollider bc = newword.AddComponent<BoxCollider> ();
+		bc.size = boxsize;
+		bc.center = boxsize / 2.0f;
+
+		wordscript.bbdim = boxsize*scale;
+
+		wordsource.Play ();
+	}
+}
---
+++ Assets/Scripts/wordActs.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class wordActs : MonoBehaviour {
+
+	private static Vector3 m_relpos = new Vector3(0.0f,1.6f,0.0f);
+	private bool m_positioned = false;
+
+	public Vector3 bbdim = new Vector3(0.0f,0.0f,0.0f);
+
+	// Use this for initialization
+	void Start () {
+		EventTrigger trigger = GetComponent<EventTrigger>( );
+		EventTrigger.Entry entry = new EventTrigger.Entry( );
+		entry.eventID = EventTriggerType.PointerEnter;
+		entry.callback.AddListener( ( data ) => { wordEnter( (PointerEventData)data ); } );
+		trigger.triggers.Add( entry );
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!m_positioned) {
+			transform.position = GvrController.ArmModel.pointerRotation * Vector3.forward + 
+				GvrController.ArmModel.pointerPosition + (m_relpos - 0.5f * bbdim);
+			transform.rotation = GvrController.ArmModel.pointerRotation;
+			if (GvrController.ClickButtonUp) {
+				m_positioned = true;
+			}
+		}
+	}
+
+	// TODO also this should be the hover callback, not enter. Need to figure out how to do that.
+	public void wordEnter( PointerEventData data ) {
+		Vector3 reticleInWord;
+		GvrLaserPointer pointer;
+		if (GvrPointerManager.Pointer.GetType () == typeof(GvrLaserPointer)) {
+			pointer = (GvrLaserPointer)GvrPointerManager.Pointer;
+			// Need to get the reticle position in word coordinates....TODO
+			reticleInWord = pointer.reticle.transform.position;
+		}
+	}
+}
