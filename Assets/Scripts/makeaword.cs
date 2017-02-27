6fa8dcb73d3e0484530da8fd90909ea7f5182733
Maria Lantin <maria.lantin@gmail.com>
2017-02-26 17:01:51 -0800

    Added some interactivity via word enter and hover.

--- Assets/Scripts/makeaword.cs
+++ Assets/Scripts/makeaword.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class makeaword : MonoBehaviour {
 
 	public GameObject alphabet;
+	public Text m_debugText;
+
+	private float m_xspace = 0;
 
 	// Use this for initialization
 	void Start () {
-
+		string ci = "i";
+		MeshFilter[] letters = alphabet.GetComponentsInChildren<MeshFilter> ();
+		Vector3 extent = new Vector3();
+		foreach (MeshFilter letter in letters) {
+			if (letter.name == ci) {
+				extent = letter.sharedMesh.bounds.extents;
+				break;
+			}
+		}
+		m_xspace = extent.x;
 	}
 	
 	// Update is called once per frame
@@ -38,7 +51,13 @@ public class makeaword : MonoBehaviour {
 		Vector3 lettercentre;
 		Vector3 extent = new Vector3();
 		Vector3 boxsize = new Vector3 ();
-		foreach (char c in word) {
+		string lowcaseWord = word.ToLower ();
+		foreach (char c in lowcaseWord) {
+			if (c == ' ') {
+				letterpos.x += m_xspace*2;
+				boxsize.x += m_xspace * 2;
+				continue;
+			}
 			foreach (MeshFilter letter in letters) {
 				if (letter.name == c.ToString()) {
 					lettermesh = Instantiate(letter) as MeshFilter;
@@ -49,7 +68,7 @@ public class makeaword : MonoBehaviour {
 					newletter.transform.localRotation = Quaternion.identity;
 					lettercentre = letter.sharedMesh.bounds.center;
 					extent = letter.sharedMesh.bounds.extents;
-					boxsize.Set (boxsize.x + extent.x * 2, Mathf.Max (boxsize.y, extent.y*2), Mathf.Max (boxsize.z, extent.z * 2));
+					boxsize.Set (boxsize.x + m_xspace + extent.x * 2, Mathf.Max (boxsize.y, extent.y*2), Mathf.Max (boxsize.z, extent.z * 2));
 					newletter.transform.localPosition = letterpos-lettercentre+extent;
 					letterpos.x += extent.x*2;
 					break;
@@ -64,7 +83,8 @@ public class makeaword : MonoBehaviour {
 		bc.size = boxsize;
 		bc.center = boxsize / 2.0f;
 
-		wordscript.bbdim = boxsize*scale;
+		wordscript.bbdim = boxsize;
+		wordscript.m_debugText = m_debugText;
 
 		wordsource.Play ();
 	}
--- Assets/Scripts/wordActs.cs
+++ Assets/Scripts/wordActs.cs
@@ -1,30 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 
-public class wordActs : MonoBehaviour {
+public class wordActs : MonoBehaviour, IGvrPointerHoverHandler, IPointerEnterHandler {
 
 	private static Vector3 m_relpos = new Vector3(0.0f,1.6f,0.0f);
 	private bool m_positioned = false;
+	private AudioSource m_wordSource;
 
 	public Vector3 bbdim = new Vector3(0.0f,0.0f,0.0f);
+	public Text m_debugText = null;
 
 	// Use this for initialization
 	void Start () {
-		EventTrigger trigger = GetComponent<EventTrigger>( );
-		EventTrigger.Entry entry = new EventTrigger.Entry( );
-		entry.eventID = EventTriggerType.PointerEnter;
-		entry.callback.AddListener( ( data ) => { wordEnter( (PointerEventData)data ); } );
-		trigger.triggers.Add( entry );
+		m_wordSource = GetComponent<AudioSource> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (!m_positioned) {
 			transform.position = GvrController.ArmModel.pointerRotation * Vector3.forward + 
-				GvrController.ArmModel.pointerPosition + (m_relpos - 0.5f * bbdim);
+				GvrController.ArmModel.pointerPosition + (m_relpos - 0.5f*transform.localScale.x * bbdim);
 			transform.rotation = GvrController.ArmModel.pointerRotation;
 			if (GvrController.ClickButtonUp) {
 				m_positioned = true;
@@ -32,14 +31,15 @@ public class wordActs : MonoBehaviour {
 		}
 	}
 
-	// TODO also this should be the hover callback, not enter. Need to figure out how to do that.
-	public void wordEnter( PointerEventData data ) {
+	public void OnGvrPointerHover(PointerEventData eventData) {
 		Vector3 reticleInWord;
-		GvrLaserPointer pointer;
-		if (GvrPointerManager.Pointer.GetType () == typeof(GvrLaserPointer)) {
-			pointer = (GvrLaserPointer)GvrPointerManager.Pointer;
-			// Need to get the reticle position in word coordinates....TODO
-			reticleInWord = pointer.reticle.transform.position;
-		}
+		Vector3 reticleLocal;
+		reticleInWord = eventData.pointerCurrentRaycast.worldPosition;
+		reticleLocal = transform.InverseTransformPoint (reticleInWord);
+		m_debugText.text = "x: " + reticleLocal.x / bbdim.x + " y: " + reticleLocal.y/bbdim.y;
+	}
+
+	public void OnPointerEnter (PointerEventData eventData) {
+		m_wordSource.Play ();
 	}
 }
