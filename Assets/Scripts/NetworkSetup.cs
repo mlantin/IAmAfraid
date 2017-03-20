b0bec027a408805a1493539d5844d9d721ac16ec
Maria Lantin <maria.lantin@gmail.com>
2017-03-19 20:17:56 -0700

    couple changes to handle network setup and spinning cube. Still not working though.

--- Assets/Scripts/NetworkSetup.cs
+++ Assets/Scripts/NetworkSetup.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class NetworkSetup : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		#if UNITY_ANDROID
+		NetworkManager.singleton.StartClient();
+		#elif UNITY_EDITOR
+		NetworkManager.singleton.StartHost();
+		#endif
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
