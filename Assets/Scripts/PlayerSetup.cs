19116d8f853675611226c66eb41459141587ed8c
Maria Lantin <maria.lantin@gmail.com>
2017-03-20 21:15:16 -0700

    This works with the spawning being aware of local player setup needs. So now there should only be one GVR controller and one MainCamera and one listener per client/scene.

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			// reparent the player hierarchy (Main Camera and GVRController) to this playerObject
+			GameObject mainplayer = GameObject.Find ("Player");
+			mainplayer.transform.parent = this.gameObject.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
