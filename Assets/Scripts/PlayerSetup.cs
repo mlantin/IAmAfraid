9baf278e30361776805f47216b05367cec1326a6
Maria Lantin <maria.lantin@gmail.com>
2017-03-21 19:29:43 -0700

    Changed the parenting of the local player. Now it’s a child of the MainCamera so we can always query a player’s position and rotation to get where it’s looking.

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -13,8 +13,8 @@ public class PlayerSetup : NetworkBehaviour {
 
 	public override void OnStartLocalPlayer() {
 		// reparent the player hierarchy (Main Camera and GVRController) to this playerObject
-		GameObject mainplayer = GameObject.Find ("Player");
-		mainplayer.transform.parent = this.gameObject.transform;
+		GameObject maincamera = GameObject.FindGameObjectWithTag("MainCamera");
+		this.gameObject.transform.parent = maincamera.transform;
 
 		// tag the local player so we can look for it later in other objects
 		gameObject.tag = "Player";
