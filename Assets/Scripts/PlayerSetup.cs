94dd4bafa07d6bf746b3f4956e89d95bd8e85d84
MEC <mirkocarich@gmail.com>
2017-04-05 22:45:11 -0700

    init mirkos commit

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -17,17 +17,22 @@ public class PlayerSetup : NetworkBehaviour {
 	}
 
 	public override void OnStartLocalPlayer() {
-		// reparent the player hierarchy (Main Camera and GVRController) to this playerObject
-		GameObject maincamera = GameObject.FindGameObjectWithTag("MainCamera");
-		this.gameObject.transform.parent = maincamera.transform;
 
 		// tag the local player so we can look for it later in other objects
 		gameObject.tag = "Player";
+
+		Debug.Log(gameObject.transform.position);
 
 		if (!isServer) {
 			//Take control of the reticle
 			GameObject reticle = GameObject.Find ("Reticle");
 			GetComponent<AuthorityManager> ().CmdAssignObjectAuthority (reticle.GetComponent<NetworkIdentity> ().netId);
+
+			// reparent the player hierarchy (Main Camera and GVRController) to this playerObject
+			GameObject maincamera = GameObject.FindGameObjectWithTag("MainCamera");
+			maincamera.transform.parent.position = this.gameObject.transform.position;
+			this.gameObject.transform.parent = maincamera.transform;
+			gameObject.transform.localPosition = Vector3.zero;
 		}
 	}
 
