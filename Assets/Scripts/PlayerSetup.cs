12f76eb2e64d9176c2ecb48dffdf52660366fc08
Maria Lantin <maria.lantin@gmail.com>
2017-04-15 15:37:44 -0700

    Now have a workable player prefab for multi-player setup! Still need to add a script for just rendering the laser on non-local players but I think it’s mostly working.

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -39,11 +39,17 @@ public class PlayerSetup : NetworkBehaviour {
 
 		//Make sure the MainCamera is the one for our local player
 		Camera currentMainCamera = Camera.main;
-		Camera playercamera = gameObject.transform.FindChild ("PlayerCamera").gameObject.GetComponent<Camera> ();
+		GameObject playerCameraObj = gameObject.transform.FindChild ("PlayerCamera").gameObject;
+		Camera playercamera = playerCameraObj.GetComponent<Camera> ();
 		playercamera.tag = "MainCamera";
-		currentMainCamera.enabled = false;
+		if (currentMainCamera != null)
+			currentMainCamera.enabled = false;
 		playercamera.enabled = true;
 
+		// Add the audiolistener, GvrAudioListener, and GvrPointerPhysicsRaycaster scripts to this object
+		playerCameraObj.AddComponent<AudioListener>();
+		playerCameraObj.AddComponent<GvrAudioListener> ();
+		playerCameraObj.AddComponent<GvrPointerPhysicsRaycaster> ();
 
 		GameObject controllerPointer = gameObject.transform.FindChild ("GvrControllerPointer").gameObject;
 		IAAInputManager inputscript = m_InputManager.GetComponent<IAAInputManager>();
@@ -59,6 +65,10 @@ public class PlayerSetup : NetworkBehaviour {
 		GvrArmModelOffsets laserscript = laser.AddComponent<GvrArmModelOffsets> ();
 		laserscript.joint = GvrArmModelOffsets.Joint.Pointer;
 
+		// Add the GvrLaserPointer script to the laser object
+		GvrLaserPointer laserPtrScript = laser.AddComponent<GvrLaserPointer> ();
+		GameObject reticle = laser.transform.FindChild ("Reticle").gameObject;
+		laserPtrScript.reticle = reticle;
 	}
 		
 }
