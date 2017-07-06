1ddb48ce2d4f2f6c832cf563ec7f35cdde7ce842
zhaohongzhu <zhaohongzhu@live.com>
2017-07-06 13:32:01 -0700

    Add two missing file

---
+++ Assets/Scripts/SceneInfo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+[System.Serializable]
+public class SceneInfoList {
+
+	public List<SceneInfo> scenes;
+	public SceneInfoList(List<SceneInfo> _sceneInfoList) {
+		scenes = _sceneInfoList;
+	}
+
+	public static SceneInfoList CreateFromJSON(string jsonText) {
+		// string json = File.ReadAllText(DATA_FILE_NAME);
+		return JsonUtility.FromJson<SceneInfoList> (jsonText);
+	}
+		
+	// Workaround for JsonUtility refusing to deserialize a root-level json array.
+	// We read the single-property object into an instance of WordInfoList, and then just take our desired list from it
+	[System.Serializable]
+	public class SceneInfo {
+		// public string name;
+		public SceneInfo(string _filename, string _name) {
+			filename = _filename;
+			name = _name;
+		}
+
+		public string filename;
+		public string name;
+
+
+	}
+
+}
