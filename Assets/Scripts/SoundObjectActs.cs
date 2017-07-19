5c6e37402a7d29446d7a85d84f130a4884c3ec56
Hongzhu Zhao <zhaohongzhu@live.com>
2017-07-19 16:35:29 -0700

    Fix refactor problem

--- Assets/Scripts/SoundObjectActs.cs
+++ Assets/Scripts/SoundObjectActs.cs
@@ -152,28 +152,28 @@ public class SoundObjectActs : NetworkBehaviour
 						m_drawingPath = true;
 					}
 				}
-			}
-		} else if (GvrController.ClickButtonUp) {
-			// We put this here because we could be releasing outside of the original target
-			if (GvrController.TouchPos.x > .85f) {
-				if (m_drawingPath) {
-					// TODO: refactor sequencer
-					tmpEndSequence();// m_sequencer.endSequence();
-					m_drawingPath = false;
-					IAAPlayer.localPlayer.CmdSetSoundObjectDrawingSequence(netId,false);
-					if (!m_looping)
-						IAAPlayer.localPlayer.CmdToggleSoundObjectLoopingState(netId);
-					if (this.GetType().Equals(typeof(WordActs))) {
-						IAAPlayer.removeAuthority(netId);
-					}
-				} else if (m_target) {
-					IAAPlayer.localPlayer.CmdToggleSoundObjectLoopingState (netId);
-					if (this.GetType().Equals(typeof(WordActs))) {
-						IAAPlayer.removeAuthority(netId);
+			} else if (GvrController.ClickButtonUp) {
+				// We put this here because we could be releasing outside of the original target
+				if (GvrController.TouchPos.x > .85f) {
+					if (m_drawingPath) {
+						// TODO: refactor sequencer
+						tmpEndSequence();// m_sequencer.endSequence();
+						m_drawingPath = false;
+						IAAPlayer.localPlayer.CmdSetSoundObjectDrawingSequence(netId,false);
+						if (!m_looping)
+							IAAPlayer.localPlayer.CmdToggleSoundObjectLoopingState(netId);
+						if (this.GetType().Equals(typeof(WordActs))) {
+							IAAPlayer.removeAuthority(netId);
+						}
+					} else if (m_target) {
+						IAAPlayer.localPlayer.CmdToggleSoundObjectLoopingState (netId);
+						if (this.GetType().Equals(typeof(WordActs))) {
+							IAAPlayer.removeAuthority(netId);
+						}
 					}
 				}
+				m_presshold = false;
 			}
-			m_presshold = false;
 		}
 		#endif
 		if (volumeChanged)
